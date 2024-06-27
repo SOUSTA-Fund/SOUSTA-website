0812bfd47acf11d813b661bb91580beb30a0e7e2
Paul Garcia <gp22@users.noreply.github.com>
2024-06-27 13:45:11 -0600

    Add layout, remove bg image, add buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,54 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import Nav from '../components/nav'
+import { graphql, Link } from 'gatsby'
+import routes from '../routes'
+import { Card } from '@material-tailwind/react'
+import Layout from '../components/layout'
 import Seo from '../components/seo'
 
 const IndexPage = ({ data }) => {
   const page = data?.wpPage
-  const src =
-    data?.wpPage?.featuredImage?.node?.localFile?.childImageSharp?.fixed?.src
 
   return (
-    <div
-      className="flex flex-col min-h-screen"
-      style={{
-        backgroundImage: `url(${src})`,
-      }}
-    >
-      <div className="flex flex-col min-h-screen bg-blue-900 bg-opacity-50">
-        <div className="bg-blue-50">
-          <Nav />
-        </div>
-        <div className="flex flex-1 items-center">
-          <main className="max-w-3xl mx-auto px-4 py-24 self-center stack text-lg md:px-8">
-            <div className="bg-blue-50 col-span-3 flex flex-col justify-center px-8 py-24 shadow-2xl">
-              {page && (
-                <>
-                  <h1 className="h1 leading-tight mb-4 lg:text-4xl">
-                    {page.title}
-                  </h1>
-                  <div
-                    className="stack text-gray-600 md:text-xl"
-                    dangerouslySetInnerHTML={{ __html: page.content }}
-                  />
-                </>
-              )}
+    <Layout variant={'wide'}>
+      <article className="stack stack-2xl">
+        {page && (
+          <>
+            <header>
+              <h1 className="lg:text-5xl">{page.title}</h1>
+            </header>
+            <Card className="p-0">
+              <div
+                className="stack"
+                dangerouslySetInnerHTML={{ __html: page.content }}
+              />
+            </Card>
+            <div className="flex flex-col gap-6 md:flex-row">
+              <Link
+                className="bg-white border border-gray-900 py-3 px-6 no-underline text-gray-900 text-sm transition-all uppercase hover:bg-yellow-600"
+                to={routes.about}
+                aria-label="about"
+              >
+                About
+              </Link>
+              <Link
+                className="bg-white border border-gray-900 py-3 px-6 no-underline text-gray-900 text-sm transition-all uppercase hover:bg-yellow-600"
+                to={routes.team}
+                aria-label="about"
+              >
+                Team
+              </Link>
+              <Link
+                className="bg-white border border-gray-900 py-3 px-6 no-underline text-gray-900 text-sm transition-all uppercase hover:bg-yellow-600"
+                to={routes.platforms}
+                aria-label="about"
+              >
+                Investment Platforms
+              </Link>
             </div>
-          </main>
-        </div>
-      </div>
-    </div>
+          </>
+        )}
+      </article>
+    </Layout>
   )
 }
 
@@ -55,13 +66,6 @@ export const query = graphql`
       featuredImage {
         node {
           altText
-          localFile {
-            childImageSharp {
-              fixed(height: 900, width: 1600) {
-                src
-              }
-            }
-          }
         }
       }
     }
