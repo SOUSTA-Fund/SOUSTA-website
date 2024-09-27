a200c3fe94fdf0f3cc9df20a92bf581b6f1d9946
Paul Garcia <gp22@users.noreply.github.com>
2024-09-27 15:20:54 -0600

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,12 @@ module.exports = {
     'gatsby-transformer-json',
     'gatsby-transformer-sharp',
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: `src/images/favicon/favicon-32x32.png`,
+      },
+    },
+    {
       resolve: `gatsby-source-wordpress`,
       options: {
         url: process.env.WPGRAPHQL_URL,
--- src/components/seo.js
+++ src/components/seo.js
@@ -5,7 +5,6 @@ const Seo = ({ title, description, pathname, children }) => {
   const {
     title: defaultTitle,
     description: defaultDescription,
-    image,
     siteUrl,
     twitterUsername,
   } = useSiteMetadata()
@@ -13,7 +12,6 @@ const Seo = ({ title, description, pathname, children }) => {
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
-    image: `${siteUrl}${image}`,
     url: `${siteUrl}${pathname || ``}`,
     twitterUsername,
   }
@@ -23,10 +21,6 @@ const Seo = ({ title, description, pathname, children }) => {
       <html lang="en" />
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
-      <link
-        rel="icon"
-        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>"
-      />
       {children}
     </>
   )
